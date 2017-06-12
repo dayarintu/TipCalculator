e33c65e3e8e2962c91c27b4a880441dc7c2cb501
Your Name <rintudhaya@gmail.com>
2017-06-12 13:42:30 -0400

    added some components

--- TipCalculator/TipCalculator/ViewController.swift
+++ TipCalculator/TipCalculator/ViewController.swift
@@ -10,16 +10,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var AmountTextField: UITextField!
+    
+    @IBOutlet weak var TipAmountTextField: UITextField!
+    
+    @IBOutlet weak var TipOutlet: UISegmentedControl!
+    
+    
+    var tipAmount = 0
+    
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
+    
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    
+    
+    @IBAction func TipPercent(_ sender: UISegmentedControl) {
+        
+        if AmountTextField != nil {
+        var enterAmount = Double(Int(AmountTextField.text!)!)
+        
+        
+        switch TipOutlet.selectedSegmentIndex {
+        case 0:
+            tipAmount = Int((enterAmount * 15.0) / Double(100))
+            TipAmountTextField.text = String(tipAmount)
+        case 1:
+            tipAmount = Int((enterAmount * 20.0) / Double(100))
+            TipAmountTextField.text = String(tipAmount)
+        case 2:
+            tipAmount = Int((enterAmount * 30.0) / Double(100))
+            TipAmountTextField.text = String(tipAmount)
+        default:
+                break
+        }
     }
-
+        else {
+            AmountTextField.text = "Enter an Amount"
+        }
+    }
+    @IBAction func Calculate(_ sender: Any) {
+    }
+    
+    
 
 }
 

a186580821257b60954f71d8c0a2bedd2d148673
Your Name <rintudhaya@gmail.com>
2017-06-16 00:19:07 -0400

    added new swift file

---
+++ TipCalculator/TipCalculator/sound.swift
@@ -0,0 +1,37 @@
+//
+//  sound.swift
+//  TipCalculator
+//
+//  Created by Daya on 6/16/17.
+//  Copyright © 2017 Daya. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+
+
+var audioPlayer1 = AVAudioPlayer()
+func calculateSound() {
+    
+    let alertSound = NSURL(fileURLWithPath: Bundle.main.path(forResource: "correct", ofType: "mp3")!)
+    
+    do {
+        try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+    } catch{
+    }
+    do {
+        try AVAudioSession.sharedInstance().setActive(true)
+    } catch{
+    }
+    
+    // Play the sound
+    do {
+        audioPlayer1 = try AVAudioPlayer(contentsOf: alertSound as URL)
+    } catch {
+    }
+    
+    audioPlayer1.prepareToPlay()
+    audioPlayer1.play()
+}
+
